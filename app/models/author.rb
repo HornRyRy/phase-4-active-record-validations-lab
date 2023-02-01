72305ef8650447458817c665541cb505e013c0b6
Ryan Horn <rj.horn@gmail.com>
2023-01-31 18:17:55 -0700

    commit tree f9c64fcfec162e4d79f1771b9a107acd1f451104

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10, message: "%{value} Should be 10 digits, everyone knows you big dummy!" }
 end
